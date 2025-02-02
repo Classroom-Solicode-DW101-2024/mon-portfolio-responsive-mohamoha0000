d12958f3614b3a733d90f52d9061398ccca5842b
mohamoha0000 <mohamedelmaeyouf@gmail.com>
2025-02-02 08:02:55 +0100

    landing page

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const hamburgerIcon = document.querySelector("header .hamburger");
 const navMenu = document.querySelector('header nav ul');
 
-
 hamburgerIcon.addEventListener('click', () => {
     
     navMenu.classList.toggle('show');
 });
+
+function showTextEffect(select,text, speed) {
+    let index = 0;
+    let element = document.querySelector(select)
+    element.innerHTML = ""; // تفريغ النص
+
+    function addCharacter() {
+        if (index < text.length) {
+            element.innerHTML += text[index];
+            index++;
+            setTimeout(addCharacter, speed);
+        }
+    }
+    
+    addCharacter();
+}
+
+showTextEffect("main .landingpage .content h2","Mohamed El Meayouf | UX Designer & Front-End Developer.",100)
