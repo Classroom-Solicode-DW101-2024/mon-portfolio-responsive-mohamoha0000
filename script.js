0c5d711d5cff3e110f12b7a035c59b9a28f20908
team c4 <83498516+mohamoha0000@users.noreply.github.com>
2025-02-04 15:24:30 +0100

    Add files via upload
    version finale

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const hamburgerIcon = document.querySelector("header .hamburger");
 const navMenu = document.querySelector('header nav ul');
-
+const ul = document.querySelectorAll("header nav ul li a");
+ul.forEach(a => {
+    a.addEventListener("click", ()=>{
+        navMenu.classList.toggle('show');
+    }) 
+});
 hamburgerIcon.addEventListener('click', () => {
     
     navMenu.classList.toggle('show');
